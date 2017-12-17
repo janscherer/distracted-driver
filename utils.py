11307058b7eb3376c272349b4d54c433c14d71fd
Jan Scherer <jan.scherer@rwth-aachen.de>
2017-12-17 18:20:40 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,19 +54,10 @@ np.set_printoptions(precision=4, linewidth=100)
 
 
 to_bw = np.array([0.299, 0.587, 0.114])
-
 def gray(img):
-    if K.image_dim_ordering() == 'tf':
-        return np.rollaxis(img, 0, 1).dot(to_bw)
-    else:
-        return np.rollaxis(img, 0, 3).dot(to_bw)
-
+    return np.rollaxis(img,0,3).dot(to_bw)
 def to_plot(img):
-    if K.image_dim_ordering() == 'tf':
-        return np.rollaxis(img, 0, 1).astype(np.uint8)
-    else:
-        return np.rollaxis(img, 0, 3).astype(np.uint8)
-
+    return np.rollaxis(img, 0, 3).astype(np.uint8)
 def plot(img):
     plt.imshow(to_plot(img))
 
@@ -82,12 +73,10 @@ def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
         if (ims.shape[-1] != 3):
             ims = ims.transpose((0,2,3,1))
     f = plt.figure(figsize=figsize)
-    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
     for i in range(len(ims)):
-        sp = f.add_subplot(rows, cols, i+1)
-        sp.axis('Off')
+        sp = f.add_subplot(rows, len(ims)//rows, i+1)
         if titles is not None:
-            sp.set_title(titles[i], fontsize=16)
+            sp.set_title(titles[i], fontsize=18)
         plt.imshow(ims[i], interpolation=None if interp else 'none')
 
 
@@ -253,8 +242,8 @@ class MixIterator(object):
     def next(self, *args, **kwargs):
         if self.multi:
             nexts = [[next(it) for it in o] for o in self.iters]
-            n0 = np.concatenate([n[0] for n in nexts])
-            n1 = np.concatenate([n[1] for n in nexts])
+            n0s = np.concatenate([n[0] for n in o])
+            n1s = np.concatenate([n[1] for n in o])
             return (n0, n1)
         else:
             nexts = [next(it) for it in self.iters]
@@ -262,3 +251,85 @@ class MixIterator(object):
             n1 = np.concatenate([n[1] for n in nexts])
             return (n0, n1)
 
+class BcolzArrayIterator(object):
+    #Thanks to R4mon: http://pastebin.com/y0DskiK6
+    def __init__(self, X, y=None, w=None, batch_size=32, shuffle=False, seed=None):
+        if y is not None and len(X) != len(y):
+            raise ValueError('X (features) and y (labels) '
+                             'should have the same length. '
+                             'Found: X.shape = %s, y.shape = %s' % (X.shape, y.shape))
+        if w is not None and len(X) != len(w):
+            raise ValueError('X (features) and w (weights) '
+                             'should have the same length. '
+                             'Found: X.shape = %s, w.shape = %s' % (X.shape, w.shape))
+        if batch_size % X.chunklen != 0:
+            raise ValueError('batch_size needs to be a multiple of X.chunklen')
+        self.chunks_per_batch = batch_size // X.chunklen
+        self.X = X
+        if y is not None:
+            self.y = y[:]
+        else:
+            self.y = None
+        if w is not None:
+            self.w = w[:]
+        else:
+            self.w = None
+        self.N = X.shape[0]
+        self.batch_size = batch_size
+        self.batch_index = 0
+        self.total_batches_seen = 0
+        self.lock = threading.Lock()
+        self.shuffle = shuffle
+        self.seed = seed
+        
+    def reset(self):
+        self.batch_index = 0
+        
+    def next(self):
+        with self.lock:
+            if self.batch_index == 0:
+                if self.seed is not None:
+                    np.random.seed(self.seed + self.total_batches_seen)
+                if self.shuffle:
+                    self.index_array = np.random.permutation(self.X.nchunks + 1)
+                else:
+                    self.index_array = np.arange(self.X.nchunks + 1)
+
+            batches_x = []
+            batches_y = []
+            batches_w = []
+            for i in range(self.chunks_per_batch):
+                current_index = self.index_array[self.batch_index]
+                if current_index == self.X.nchunks:
+                    batches_x.append(self.X.leftover_array[:self.X.leftover_elements])
+                    current_batch_size = self.X.leftover_elements
+                else:
+                    batches_x.append(self.X.chunks[current_index][:])
+                    current_batch_size = self.X.chunklen
+                self.batch_index += 1
+                self.total_batches_seen += 1
+                if not self.y is None:
+                    batches_y.append(self.y[current_index * self.X.chunklen: current_index * self.X.chunklen + current_batch_size])
+                if not self.w is None:
+                    batches_w.append(self.w[current_index * self.X.chunklen: current_index * self.X.chunklen + current_batch_size])
+                if self.batch_index >= len(self.index_array):
+                    self.batch_index = 0
+                    break
+            
+        batch_x = np.concatenate(batches_x)
+        if self.y is None:
+            return batch_x
+        
+        batch_y = np.concatenate(batches_y)
+        if self.w is None:
+            return batch_x, batch_y
+        
+        batch_w = np.concatenate(batches_w)
+        return batch_x, batch_y, batch_w
+
+    def __iter__(self):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        return self.next(*args, **kwargs)
+        
